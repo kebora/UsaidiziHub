83ca889f5fcbae69ecdaa555e7440c008cf49007
kebora <danielkebora@gmail.com>
2023-03-03 14:03:25 +0300

    add a theme

--- app/src/main/java/com/simiyu/usaidizihub/MainFragment.java
+++ app/src/main/java/com/simiyu/usaidizihub/MainFragment.java
@@ -3,10 +3,13 @@ package com.simiyu.usaidizihub;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class MainFragment extends Fragment {
     View view;
@@ -16,7 +19,13 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_main, container, false);
-
+        FrameLayout rowsFragment = view.findViewById(R.id.frame_layout_home_row);
+        //Add the fragment
+        RowFragment rowFragment = new RowFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout_home_row, rowFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
         return view;
     }
 }
