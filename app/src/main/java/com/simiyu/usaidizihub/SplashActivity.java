f17cdd2fbf7e222296f71b35beb2568abd7d12ba
kebora <danielkebora@gmail.com>
2023-03-04 11:39:31 +0300

    intent to login activity

--- app/src/main/java/com/simiyu/usaidizihub/SplashActivity.java
+++ app/src/main/java/com/simiyu/usaidizihub/SplashActivity.java
@@ -3,6 +3,7 @@ package com.simiyu.usaidizihub;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -10,15 +11,29 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
     private ImageView imageViewBackground;
+    private Button emailAndPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         removeActionBar();
+
+        //When the user selects email and password
+        //
+        emailAndPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
     // Remove the action bar
     void removeActionBar() {
