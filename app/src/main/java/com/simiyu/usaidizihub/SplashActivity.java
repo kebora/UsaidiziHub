410f4b2705a63358e377f9dbbf034b6258c83647
kebora <danielkebora@gmail.com>
2023-03-06 04:33:43 +0300

    add fade animation for razor

--- app/src/main/java/com/simiyu/usaidizihub/SplashActivity.java
+++ app/src/main/java/com/simiyu/usaidizihub/SplashActivity.java
@@ -4,20 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
     private ImageView imageViewBackground;
     private Button emailAndPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +28,19 @@ public class SplashActivity extends AppCompatActivity {
         emailAndPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
 
+        //Add the main-text animation
+        TextView myTextView = findViewById(R.id.textview_razor_studios);
+        Animation myAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_text_animation);
+        myTextView.startAnimation(myAnimation);
+
+
     }
+
     // Remove the action bar
     void removeActionBar() {
         ActionBar actionBar = getSupportActionBar();
