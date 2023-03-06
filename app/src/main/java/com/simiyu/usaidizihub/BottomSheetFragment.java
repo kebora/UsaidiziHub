e321b73e3af9d6e5ac260b698d13db6ca7d6a6c7
kebora <danielkebora@gmail.com>
2023-03-06 11:39:50 +0300

    add BottomSheetFragment

---
+++ app/src/main/java/com/simiyu/usaidizihub/BottomSheetFragment.java
@@ -0,0 +1,22 @@
+package com.simiyu.usaidizihub;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login_bottom_sheet, container, false);
+        LinearLayout bottomTextView = view.findViewById(R.id.bottom_sheet);
+        return view;
+    }
+}
--- app/src/main/java/com/simiyu/usaidizihub/LoginActivity.java
+++ app/src/main/java/com/simiyu/usaidizihub/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.simiyu.usaidizihub;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +14,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         removeActionBar();
+
+        // initialize the image button to open the bottom sheet
+        ImageButton resetPasswordButton = findViewById(R.id.button_reset_password);
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetFragment bottomSheet = new BottomSheetFragment();
+                bottomSheet.show(getSupportFragmentManager(), "BottomSheet");
+            }
+        });
     }
     // Remove the action bar
     void removeActionBar() {
